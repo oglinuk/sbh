e83cfb8358b13b35c0faf5448a157d5f6011fd3c
oglinuk <daniel-l-black@protonmail.com>
2022-03-11 06:27:28 -0800

    Remove `stretchr/testify`
    Removed the use of `testify`, as the only thing is was used for is
    string comparison. There are now no external dependencies.

--- examples/README.md
+++ examples/README.md
@@ -2,7 +2,7 @@
 
 ## TODO
 
-* [X] Create CLI implementation
-* [X] Create docker implementation
-* [X] Create web UI implementation
-* [X] Create REST API implementation
+* [X] Create [CLI](cli) implementation
+* [X] Create [docker](docker) implementation
+* [X] Create [web UI](web) implementation
+* [X] Create [REST API](rest) implementation
--- sbh_test.go
+++ sbh_test.go
@@ -2,8 +2,6 @@ package sbh
 
 import (
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func getSBH(algo, pt, symbols string, nr, seed int64, ut, l int) SBH {
@@ -22,61 +20,81 @@ func getSBH(algo, pt, symbols string, nr, seed int64, ut, l int) SBH {
 func TestBadInputSBH(t *testing.T) {
 	expected := "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
 	actual := Generate(SBH{})
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestDefaultGenerate(t *testing.T) {
 	expected := "505c3c3fab111175642a3073756472621d0312388dec72ee9268c05f96463ecb"
 	actual := Generate(getSBH("", "test", "", 1729, 42, 0, 0))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSymbolsGenerate(t *testing.T) {
 	expected := "505c3!@#$%^&" //c3fab111175642a3073756472621d0312388dec72ee9268c05f96463ecb!@#$%^&"
 	actual := Generate(getSBH("", "test", "!@#$%^&", 1729, 42, 0, 12))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestUppercaseTimesGenerate(t *testing.T) {
 	expected := "505C3C3Fab111" //175642a3073756472621d0312388dec72ee9268c05f96463ecb"
 	actual := Generate(getSBH("", "test", "", 1729, 42, 3, 13))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSHA256_224Generate(t *testing.T) {
 	expected := "8abc1cdbbc47d3" //e3876f86fccd0715a956177ea181249621f90a6222"
 	actual := Generate(getSBH("sha256_224", "test", "", 1729, 42, 0, 14))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSHA256Generate(t *testing.T) {
 	expected := "505c3c3fab11117" //5642a3073756472621d0312388dec72ee9268c05f96463ecb"
 	actual := Generate(getSBH("sha256", "test", "", 1729, 42, 0, 15))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSHA512_224Generate(t *testing.T) {
 	expected := "62ff96f2cab0dc8c" //e549b0ba9cefe079532bb5143897f4ec7b4b66c9"
 	actual := Generate(getSBH("sha512_224", "test", "", 1729, 42, 0, 16))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSHA512_256Generate(t *testing.T) {
 	expected := "814e45a6755086fe0" //9ac2f52c1412154fa6d5048ff26b27b9393b20149243a9a"
 	actual := Generate(getSBH("sha512_256", "test", "", 1729, 42, 0, 17))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSHA512_384Generate(t *testing.T) {
 	expected := "58014bc206308721b4" //575ca55800be7a4c54ca7f8c65a35d5b1271dee7e9ce89eaf750eca6af4d4b95aa32448a412490"
 	actual := Generate(getSBH("sha512_384", "test", "", 1729, 42, 0, 18))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func TestSHA512Generate(t *testing.T) {
 	expected := "8be80abeb9fa101f7e6" //7b4d7da5ce00b1914e32d361eb67627d6fd4fd85cadf13a0bb079332508f443445a8ce410c1664e9bfcfcd86e6c7ca47cf47d234d7d49"
 	actual := Generate(getSBH("sha512", "test", "", 1729, 42, 0, 19))
-	assert.Equal(t, expected, actual)
+	if actual != expected {
+		t.Errorf("\nExpected: %s\n  Actual: %s\n", expected, actual)
+	}
 }
 
 func benchmarkDefaultGenerate(nRots int64, b *testing.B) {
